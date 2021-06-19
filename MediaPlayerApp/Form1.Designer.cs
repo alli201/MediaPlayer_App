02f1a09aa3772d860e723ababed05f69015a5be3
alli201 <80549305+alli201@users.noreply.github.com>
2021-06-20 00:03:25 +0300

    Add files via upload

---
+++ MediaPlayerApp/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace MediaPlayerApp
+{
+    partial class MediaPlayerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MediaPlayerApp));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMin = new System.Windows.Forms.PictureBox();
+            this.pictureBoxMax = new System.Windows.Forms.PictureBox();
+            this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.btnSelectMedia = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.picNext = new System.Windows.Forms.PictureBox();
+            this.picPrevious = new System.Windows.Forms.PictureBox();
+            this.picStop = new System.Windows.Forms.PictureBox();
+            this.PicPlayPause = new System.Windows.Forms.PictureBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMax)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPrevious)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicPlayPause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel1.Controls.Add(this.pictureBoxLogo);
+            this.panel1.Controls.Add(this.pictureBoxMin);
+            this.panel1.Controls.Add(this.pictureBoxMax);
+            this.panel1.Controls.Add(this.pictureBoxClose);
+            this.panel1.Controls.Add(this.lblLogo);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Name = "panel1";
+            this.panel1.DoubleClick += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.pictureBoxLogo, "pictureBoxLogo");
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // pictureBoxMin
+            // 
+            resources.ApplyResources(this.pictureBoxMin, "pictureBoxMin");
+            this.pictureBoxMin.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.pictureBoxMin.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBoxMin.Name = "pictureBoxMin";
+            this.pictureBoxMin.TabStop = false;
+            this.pictureBoxMin.Click += new System.EventHandler(this.pictureBox3_Click);
+            this.pictureBoxMin.MouseEnter += new System.EventHandler(this.pictureBoxMin_MouseEnter);
+            this.pictureBoxMin.MouseLeave += new System.EventHandler(this.pictureBoxMin_MouseLeave);
+            // 
+            // pictureBoxMax
+            // 
+            resources.ApplyResources(this.pictureBoxMax, "pictureBoxMax");
+            this.pictureBoxMax.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.pictureBoxMax.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBoxMax.Name = "pictureBoxMax";
+            this.pictureBoxMax.TabStop = false;
+            this.pictureBoxMax.Click += new System.EventHandler(this.pictureBox2_Click);
+            this.pictureBoxMax.MouseEnter += new System.EventHandler(this.pictureBoxMax_MouseEnter);
+            this.pictureBoxMax.MouseLeave += new System.EventHandler(this.pictureBoxMax_MouseLeave);
+            // 
+            // pictureBoxClose
+            // 
+            resources.ApplyResources(this.pictureBoxClose, "pictureBoxClose");
+            this.pictureBoxClose.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.pictureBoxClose.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.pictureBoxClose.Name = "pictureBoxClose";
+            this.pictureBoxClose.TabStop = false;
+            this.pictureBoxClose.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBoxClose.MouseEnter += new System.EventHandler(this.pictureBoxClose_MouseEnter);
+            this.pictureBoxClose.MouseLeave += new System.EventHandler(this.pictureBoxClose_MouseLeave);
+            // 
+            // lblLogo
+            // 
+            resources.ApplyResources(this.lblLogo, "lblLogo");
+            this.lblLogo.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.lblLogo.Name = "lblLogo";
+            // 
+            // listBoxSongs
+            // 
+            resources.ApplyResources(this.listBoxSongs, "listBoxSongs");
+            this.listBoxSongs.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.listBoxSongs.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBoxSongs.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.listBoxSongs.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            // 
+            // btnSelectMedia
+            // 
+            resources.ApplyResources(this.btnSelectMedia, "btnSelectMedia");
+            this.btnSelectMedia.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSelectMedia.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnSelectMedia.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnSelectMedia.FlatAppearance.BorderSize = 7;
+            this.btnSelectMedia.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnSelectMedia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btnSelectMedia.Name = "btnSelectMedia";
+            this.btnSelectMedia.UseVisualStyleBackColor = false;
+            this.btnSelectMedia.Click += new System.EventHandler(this.btnSelectSongs_Click);
+            // 
+            // btnDelete
+            // 
+            resources.ApplyResources(this.btnDelete, "btnDelete");
+            this.btnDelete.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Default;
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnDelete.FlatAppearance.BorderSize = 7;
+            this.btnDelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DimGray;
+            this.btnDelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Black;
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.picNext);
+            this.panel2.Controls.Add(this.picPrevious);
+            this.panel2.Controls.Add(this.picStop);
+            this.panel2.Controls.Add(this.PicPlayPause);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.panel2.Name = "panel2";
+            // 
+            // picNext
+            // 
+            resources.ApplyResources(this.picNext, "picNext");
+            this.picNext.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picNext.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picNext.Name = "picNext";
+            this.picNext.TabStop = false;
+            this.picNext.Click += new System.EventHandler(this.picNext_Click);
+            // 
+            // picPrevious
+            // 
+            resources.ApplyResources(this.picPrevious, "picPrevious");
+            this.picPrevious.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picPrevious.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picPrevious.Name = "picPrevious";
+            this.picPrevious.TabStop = false;
+            this.picPrevious.Click += new System.EventHandler(this.picPrevious_Click);
+            // 
+            // picStop
+            // 
+            resources.ApplyResources(this.picStop, "picStop");
+            this.picStop.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picStop.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picStop.Name = "picStop";
+            this.picStop.TabStop = false;
+            this.picStop.Click += new System.EventHandler(this.picStop_Click);
+            // 
+            // PicPlayPause
+            // 
+            resources.ApplyResources(this.PicPlayPause, "PicPlayPause");
+            this.PicPlayPause.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PicPlayPause.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PicPlayPause.Name = "PicPlayPause";
+            this.PicPlayPause.TabStop = false;
+            this.PicPlayPause.Click += new System.EventHandler(this.PicPlayPause_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.AllowDrop = true;
+            resources.ApplyResources(this.axWindowsMediaPlayer1, "axWindowsMediaPlayer1");
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.axWindowsMediaPlayer1_PlayStateChange);
+            // 
+            // MediaPlayerApp
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnSelectMedia);
+            this.Controls.Add(this.listBoxSongs);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MediaPlayerApp";
+            this.Load += new System.EventHandler(this.MediaPlayerApp_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMax)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picNext)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPrevious)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picStop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicPlayPause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.PictureBox pictureBoxClose;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.PictureBox pictureBoxMin;
+        private System.Windows.Forms.PictureBox pictureBoxMax;
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.Button btnSelectMedia;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox PicPlayPause;
+        private System.Windows.Forms.PictureBox picStop;
+        private System.Windows.Forms.PictureBox picNext;
+        private System.Windows.Forms.PictureBox picPrevious;
+    }
+}
+
---
+++ MediaPlayerApp/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Reflection;
+
+namespace MediaPlayerApp
+{
+    public partial class MediaPlayerApp : Form
+    {
+        
+        public MediaPlayerApp()
+        {
+            InitializeComponent();
+        }
+        int maxFlag = 1;   //for maxmize the app
+
+        //for storing the name and the path of the files:
+        List<string> filesname = new List<string>();
+        List<string> paths = new List<string>();
+        private void btnSelectSongs_Click(object sender, EventArgs e)
+        {
+
+                OpenFileDialog o = new OpenFileDialog();  //creating a dialog for selecting files
+                o.Multiselect = true;             // so that we can select more than one file in the same time
+
+            o.Filter="Media|*.mp4;*.mp3";
+                if (o.ShowDialog() == System.Windows.Forms.DialogResult.OK)   //after we select the files
+                {
+                    filesname.Clear();   //to prevent dublicating files names
+                    filesname.AddRange(o.SafeFileNames);
+
+                    paths.AddRange(o.FileNames);
+
+                    for (int i = 0; i < filesname.Count; i++)
+                    {
+                        listBoxSongs.Items.Add(filesname[i]);       //toadd the files to the listBox
+                    }
+                }
+            }
+
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+         try
+            {
+            axWindowsMediaPlayer1.URL = paths[listBoxSongs.SelectedIndex];   //to play the media that we select from the listBox
+            }
+
+         catch { }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (maxFlag == 1) { 
+            WindowState = FormWindowState.Maximized;       //in the bigening if we click it the app will maximize
+                maxFlag = 0;
+                           }
+           else if (maxFlag == 0)
+            {
+                WindowState = FormWindowState.Normal;      // after it is maximized if we click the app size will back to normal
+                maxFlag = 1;
+            }
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;        // for minimizzing the app
+        }
+
+
+        private void MediaPlayerApp_Load(object sender, EventArgs e)
+        {
+            PicPlayPause.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\alikh\Desktop\C# Projects\MediaPlayerApp\pictures\play.png");
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Close();      //for closing the app
+        }
+
+
+        private void pictureBoxClose_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBoxClose.BackColor = Color.Gray;
+        }
+
+    
+
+        private void pictureBoxClose_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBoxClose.BackColor = Color.CornflowerBlue;
+        }
+
+        private void pictureBoxMax_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBoxMax.BackColor = Color.Gray;
+        }
+
+        private void pictureBoxMax_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBoxMax.BackColor = Color.CornflowerBlue;
+        }
+
+        private void pictureBoxMin_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBoxMin.BackColor = Color.Gray;
+        }
+
+        private void pictureBoxMin_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBoxMin.BackColor = Color.CornflowerBlue;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxSongs.Items.Count > 0) {    //if the listbox is not empty
+                int n = listBoxSongs.Items.Count;  // holding the current number of items in the listbox
+            try
+            {
+                paths.Remove(paths[listBoxSongs.SelectedIndex]);       //to remove the path of the selected item
+                listBoxSongs.Items.RemoveAt(listBoxSongs.SelectedIndex);   //to remove the selected item from the listbox                
+                //filesname.Remove(filesname[listBoxSongs.SelectedIndex]);   //to remove the file name of the selected item
+
+                }
+                catch
+            {}
+                finally
+                {
+                    if (listBoxSongs.Items.Count < n)                //if an item have been removed
+                        MessageBox.Show("Deleted Successfully!");
+                }
+                                             }
+        }
+
+
+        private void PicPlayPause_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.pause();
+                PicPlayPause.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\alikh\Desktop\C# Projects\MediaPlayerApp\pictures\play.png");
+                PicPlayPause.BackgroundImageLayout = ImageLayout.Stretch;
+            }
+             else
+            if(axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPaused || axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+                PicPlayPause.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\alikh\Desktop\C# Projects\MediaPlayerApp\pictures\pause.png");
+
+            }
+        
+        }
+
+        private void picStop_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+
+                picStop.Size = new Size(25, 25);
+
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+            }
+        }
+
+        private void axWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+              if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                PicPlayPause.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\alikh\Desktop\C# Projects\MediaPlayerApp\pictures\pause.png");
+            }
+              if(axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsStopped)
+                PicPlayPause.BackgroundImage = System.Drawing.Image.FromFile(@"C:\Users\alikh\Desktop\C# Projects\MediaPlayerApp\pictures\play.png");
+
+              if(axWindowsMediaPlayer1.playState != WMPLib.WMPPlayState.wmppsStopped)
+                picStop.Size = new Size(30, 30);
+        }
+
+        private void picPrevious_Click(object sender, EventArgs e)
+        {
+            if(listBoxSongs.SelectedIndex!=listBoxSongs.TopIndex)   // if the pointer is not on the first (top) item.
+            try
+            {
+                axWindowsMediaPlayer1.URL = paths[--listBoxSongs.SelectedIndex];
+            }
+            catch { }
+        }
+
+        private void picNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                axWindowsMediaPlayer1.URL = paths[++listBoxSongs.SelectedIndex];
+            }
+            catch { }
+        }
+    }
+}
